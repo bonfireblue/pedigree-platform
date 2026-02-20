10c1c31b122b511fd0c88cbac52067cb582d61fd
Bon_Huynh <bonfireblue@gmail.com>
2026-02-20 12:44:27 -0800

    Redirect home to sign-in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Family Tree App</h1>
-      <p>If you can see this, Day 1 is working.</p>
-    </main>
-  );
+  redirect("/sign-in");
 }
